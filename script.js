bb4a46200a22c295983e0d831cdfd4c92cee1115
doebkblcya <2738103877@qq.com>
2024-11-30 19:05:37 +0800

    1

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
     sections.forEach((section, index) => {
         section.style.animationDelay = `${index * 0.5}s`;
+        section.classList.add('fade-in');
     });
 });
